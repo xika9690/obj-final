64ad50bdbed90a6f72475b8a7365c6a02a26ae43
xika9690 <xika9690@colorado.edu>
2022-12-06 16:44:00 -0700

    h

--- obj-final.ino
+++ obj-final.ino
@@ -13,24 +13,45 @@
 //#define PIN5 13
 
 //the first parameter is the number of LEDs you have - change for your strip
-Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(1, PIN1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(1, PIN2, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(1, PIN3, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip4 = Adafruit_NeoPixel(1, PIN4, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel strip5 = Adafruit_NeoPixel(1, PIN5, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(1, PIN1, NEO_GRB + NEO_KHZ800); // r1
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(1, PIN2, NEO_GRB + NEO_KHZ800); // r2
+Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(1, PIN3, NEO_GRB + NEO_KHZ800); // r3
+Adafruit_NeoPixel strip4 = Adafruit_NeoPixel(1, PIN4, NEO_GRB + NEO_KHZ800); // r4
+//Adafruit_NeoPixel strip5 = Adafruit_NeoPixel(1, PIN5, NEO_GRB + NEO_KHZ800); // r5
 
 //pin 2 is SEND pin - common for all | cs_commonPin#_touchPin
-CapacitiveSensor cs_2_52 = CapacitiveSensor(2, 52);
-CapacitiveSensor cs_2_53 = CapacitiveSensor(2, 53);
-CapacitiveSensor cs_2_50 = CapacitiveSensor(2, 50);
-CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51);
+CapacitiveSensor cs_2_52 = CapacitiveSensor(2, 52); // r1c1
+CapacitiveSensor cs_2_53 = CapacitiveSensor(2, 53); // r1c2
+CapacitiveSensor cs_2_50 = CapacitiveSensor(2, 50); // r1c3
+CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r1c4
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r1c5
+//CapacitiveSensor cs_2_52 = CapacitiveSensor(2, 52); // r2c1
+//CapacitiveSensor cs_2_53 = CapacitiveSensor(2, 53); // r2c2
+//CapacitiveSensor cs_2_50 = CapacitiveSensor(2, 50); // r2c3
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r2c4
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r2c5
+//CapacitiveSensor cs_2_52 = CapacitiveSensor(2, 52); // r3c1
+//CapacitiveSensor cs_2_53 = CapacitiveSensor(2, 53); // r3c2
+//CapacitiveSensor cs_2_50 = CapacitiveSensor(2, 50); // r3c3
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r3c4
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r3c5
+//CapacitiveSensor cs_2_52 = CapacitiveSensor(2, 52); // r4c1
+//CapacitiveSensor cs_2_53 = CapacitiveSensor(2, 53); // r4c2
+//CapacitiveSensor cs_2_50 = CapacitiveSensor(2, 50); // r4c3
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r4c4
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r4c5
+//CapacitiveSensor cs_2_52 = CapacitiveSensor(2, 52); // r5c1
+//CapacitiveSensor cs_2_53 = CapacitiveSensor(2, 53); // r5c2
+//CapacitiveSensor cs_2_50 = CapacitiveSensor(2, 50); // r5c3
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r5c4
+//CapacitiveSensor cs_2_51 = CapacitiveSensor(2, 51); // r5c5
 
 // variables
 int ledPin1 =  42; // the number of the LED pin
 int ledPin2 =  38; // the number of the LED pin
 int ledPin3 =  34; // the number of the LED pin
 int ledPin4 =  30; // the number of the LED pin
-//int ledPin5 =  13; // the number of the LED pin
+int ledPin5 =  13; // the number of the LED pin
 //int potPin = A6; // brightness
 //int potVal = 0; // save the value of potentiometer
 //int potPin2 = A4; // color mode
@@ -53,6 +74,8 @@ void setup() {
   strip3.show(); // Initialize all pixels to 'off'
   strip4.begin(); //we always need to do this
   strip4.show(); // Initialize all pixels to 'off'
+//  strip5.begin(); //we always need to do this
+//  strip5.show(); // Initialize all pixels to 'off'
 
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -60,50 +83,37 @@ void setup() {
 
 void loop() {
   // sensor stuff cs_commonPin#_touchPin#
-  long capSense1 =  cs_2_52.capacitiveSensor(30);
-  long capSense2 =  cs_2_53.capacitiveSensor(30);
-  long capSense3 =  cs_2_50.capacitiveSensor(30);
-  long capSense4 =  cs_2_51.capacitiveSensor(30);
-//  long capSense5 =  cs_2_3.capacitiveSensor(30);
-//  long capSense6 =  cs_2_4.capacitiveSensor(30);
-//  long capSense7 =  cs_2_5.capacitiveSensor(30);
-//  long capSense8 =  cs_2_6.capacitiveSensor(30);
-//  long capSense9 =  cs_2_3.capacitiveSensor(30);
-//  long capSense10 =  cs_2_4.capacitiveSensor(30);
-//  long capSense11 =  cs_2_5.capacitiveSensor(30);
-//  long capSense12 =  cs_2_6.capacitiveSensor(30);
-//  long capSense13 =  cs_2_3.capacitiveSensor(30);
-//  long capSense14 =  cs_2_4.capacitiveSensor(30);
-//  long capSense15 =  cs_2_5.capacitiveSensor(30);
-//  long capSense16 =  cs_2_6.capacitiveSensor(30);
-//  long capSense17 =  cs_2_3.capacitiveSensor(30);
-//  long capSense18 =  cs_2_4.capacitiveSensor(30);
-//  long capSense19 =  cs_2_5.capacitiveSensor(30);
-//  long capSense20 =  cs_2_6.capacitiveSensor(30);
-//  long capSense21 =  cs_2_3.capacitiveSensor(30);
-//  long capSense22 =  cs_2_4.capacitiveSensor(30);
-//  long capSense23 =  cs_2_5.capacitiveSensor(30);
-//  long capSense24 =  cs_2_6.capacitiveSensor(30);
-//  long capSense25 =  cs_2_6.capacitiveSensor(30);
-
-//  long capSense1 =  cs_2_7.capacitiveSensor(30);
-//  long capSense2 =  cs_2_8.capacitiveSensor(30);
-//  long capSense3 =  cs_2_9.capacitiveSensor(30);
-//  long capSense4 =  cs_2_10.capacitiveSensor(30);
+  long capSense1 =  cs_2_52.capacitiveSensor(30); // r1c1
+  long capSense2 =  cs_2_53.capacitiveSensor(30); // r1c2
+  long capSense3 =  cs_2_50.capacitiveSensor(30); // r1c3
+  long capSense4 =  cs_2_51.capacitiveSensor(30); // r1c4
+//  long capSense5 =  cs_2_3.capacitiveSensor(30); // r1c5
+//  long capSense6 =  cs_2_4.capacitiveSensor(30); // r2c1
+//  long capSense7 =  cs_2_5.capacitiveSensor(30); // r2c2
+//  long capSense8 =  cs_2_6.capacitiveSensor(30); // r2c3
+//  long capSense9 =  cs_2_3.capacitiveSensor(30); // r2c4
+//  long capSense10 =  cs_2_4.capacitiveSensor(30); // r2c5
+//  long capSense11 =  cs_2_5.capacitiveSensor(30); // r3c1
+//  long capSense12 =  cs_2_6.capacitiveSensor(30); // r3c2
+//  long capSense13 =  cs_2_3.capacitiveSensor(30); // r3c3
+//  long capSense14 =  cs_2_4.capacitiveSensor(30); // r3c4
+//  long capSense15 =  cs_2_5.capacitiveSensor(30); // r3c5
+//  long capSense16 =  cs_2_6.capacitiveSensor(30); // r4c1
+//  long capSense17 =  cs_2_3.capacitiveSensor(30); // r4c2
+//  long capSense18 =  cs_2_4.capacitiveSensor(30); // r4c3
+//  long capSense19 =  cs_2_5.capacitiveSensor(30); // r4c4
+//  long capSense20 =  cs_2_6.capacitiveSensor(30); // r4c5
+//  long capSense21 =  cs_2_3.capacitiveSensor(30); // r5c1
+//  long capSense22 =  cs_2_4.capacitiveSensor(30); // r5c2
+//  long capSense23 =  cs_2_5.capacitiveSensor(30); // r5c3
+//  long capSense24 =  cs_2_6.capacitiveSensor(30); // r5c4
+//  long capSense25 =  cs_2_6.capacitiveSensor(30); // r5c5
 
   // LED stuff
   if(capSense1 > 100) {
       strip1.setPixelColor(0, 255, 0, 0); //r
       strip1.setBrightness(50);
       strip1.show();
-//      delay(500);
-            
-//      //turn off on one at a time
-//      for(uint16_t i=0; i<strip.numPixels(); i++) {
-//          strip.setPixelColor(i, 0, 0, 0); //set to purple, takes RGB vals 0-255
-//          strip.show();
-//          delay(500);
-//          }
       }
   else {
       // turn LED off:
@@ -117,8 +127,6 @@ void loop() {
       strip2.setPixelColor(0, 0, 255, 0); //g
       strip2.setBrightness(50);
       strip2.show();
-      delay(500);
-
       }
   else {
       // turn LED off:
@@ -154,6 +162,18 @@ void loop() {
       }
   }
 
+//  if(capSense5 > 100) {
+//      strip4.setPixelColor(0, 255, 255, 255); //w
+//      strip4.setBrightness(50);
+//      strip4.show();
+//      }
+//  else {
+//      // turn LED off:
+//      for(uint16_t i=0; i<strip4.numPixels(); i++) {
+//          strip4.setPixelColor(i, 0, 0, 0); //off!!!
+//          strip4.show();
+//      }
+//  }
   // serial monitor code
   Serial.print(capSense1); //print raw sensor value
   Serial.print("    "); //print raw sensor value
@@ -162,6 +182,48 @@ void loop() {
   Serial.print(capSense3); //print raw sensor value
   Serial.print("    "); //print raw sensor value
   Serial.println(capSense4); //print raw sensor value
+//  Serial.print("    ");
+//  Serial.print(capSense5);
+//  Serial.print("    ");
+//  Serial.print(capSense6);
+//  Serial.print("    ");
+//  Serial.print(capSense7);
+//  Serial.print("    ");
+//  Serial.print(capSense8);
+//  Serial.print("    ");
+//  Serial.print(capSense9);
+//  Serial.print("    ");
+//  Serial.print(capSense10);
+//  Serial.print("    ");
+//  Serial.print(capSense11);
+//  Serial.print("    ");
+//  Serial.print(capSense12);
+//  Serial.print("    ");
+//  Serial.print(capSense13);
+//  Serial.print("    ");
+//  Serial.print(capSense14);
+//  Serial.print("    ");
+//  Serial.print(capSense15);
+//  Serial.print("    ");
+//  Serial.print(capSense16);
+//  Serial.print("    ");
+//  Serial.print(capSense17);
+//  Serial.print("    ");
+//  Serial.print(capSense18);
+//  Serial.print("    ");
+//  Serial.print(capSense19);
+//  Serial.print("    ");
+//  Serial.print(capSense20);
+//  Serial.print("    ");
+//  Serial.print(capSense21);
+//  Serial.print("    ");
+//  Serial.print(capSense22);
+//  Serial.print("    ");
+//  Serial.print(capSense23);
+//  Serial.print("    ");
+//  Serial.print(capSense24);
+//  Serial.print("    ");
+//  Serial.println(capSense25);
 
   delay(30); // arbitrary delay to limit data to serial port
 }
